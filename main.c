840518b9318e1a1468ff0ca82ba9010916eb290c
Roni <roninu@metropolia.fi>
2023-12-07 11:41:35 +0200

    Cleaned the main for the project's operation. Wrote interpretations of the required operations. There are a few question marks that ought to be discussed / decided on ; I found the instructions somewhat ambigious on them.

--- main.c
+++ main.c
@@ -1,56 +1,66 @@
-#include <stdio.h>
 #include "hardware/i2c.h"
 #include "pico/stdlib.h"
 
+#include "LED/LED.h"
 #include "stepper.h"
 #include "opto.h"
-#include "command.h"
 
 int main() {
     stdio_init_all();
 
+    LED led = {20};
+    init_led(&led);
     init_opto_fork();
     init_stepper();
 
-    /// CALIBRATE ///
-    // ... pill cabinet position.
-    // 'Count the number of steps per revolution
-    // by using an optical sensor.'
-    // 'Reducer gear ratio is NOT EXACTLY 1:64'
-    // so steps per revolution is NOT 4096 either...???
-
-    ////// USER-INPUT //////
-    /// calib /// initiates calibration
-    // Run motor in one direction,
-    // counting steps,
-    // until opto detects falling edge.
-    // 'For accuracy, count steps x3 and take average.'
-    /// status /// print status according to whether calibrated
-    // stepper_is_calibrated()
-    // Steps per revolution if calibrated...
-    // ... "Unknown" if not calibrated.
-    /// run N /// run motor according to N
-    // Motor runs N 1/8th's -- N == number of 'cabinet steps'.
-    // In case N is omitted, run full revolution.
-
-    char input[MAX_CMD_LEN + EXCESS_C + NULLC];
-    int steps_per_rev = -1;
-    int octets;
-    printf("Waiting for command...\n");
     while (true) {
-        st state = valid_command_received(input, &octets);
-        if (state != WAIT) {
-            switch (state) {
-                case CALIBRATE:
-                    steps_per_rev = calibrate(); break;
-                case STATUS:
-                    print_status(steps_per_rev); break;
-                case RUN:
-                    rotate_octets(steps_per_rev, octets); break;
-                default:
-                    fprintf(stderr, "Stderr herr derr\n");
-            }
-            printf("\nWaiting for command...\n");
-        }
+        /// AT STARTUP ///
+        /// Minimum:
+        // Blink LED,
+        // Until button pressed
+        /// Advanced:
+        // Remembers state across boot/power down.
+        // LoRaWAN Report: boot.
+
+        /// BUTTON PRESSED #1 ///
+        /// Minimum:
+        // Calibrate,
+        // 'At least one full "turn" (revolution?)',
+        // Stops at opto-fork,
+        // LED turns on
+        /// Advanced:
+        // Calibrate according to state:
+        // opposite direction so the pills aren't dispensed.
+        // LoRaWAN Report: Calibration started.
+
+        /// AFTER INITIAL CALIBRATION ///
+        /// Minimum:
+        // [fill pill slots]
+        // Waits for button press
+        /// Advanced:
+        // ? Dispense pill if previous dispensing was not finished due to reboot ?
+        // LoRaWAN Report: Calibration finished.
+
+        /// BUTTON PRESSED #2 ///
+        /// Minimum:
+        // Start dispensing pills at X intervals (x = 30 s),
+        /// Advanced:
+        // LoRaWAN Report: Dispensing started.
+
+        /// PILL DROP ///
+        /// Minimum:
+        // Detect pill drop with piezo-sensor (counting steps isn't mentioned... however:),
+        // If no pill was detected (within a certain number of steps?), blink LED 5 times
+        /// Advanced:
+        // LoRaWAN Report: Dropping pill...
+        // LoRaWAN Report: Pill drop detected || not detected. (Again, determined how?)
+
+        /// WHEEL TURNED 7 TIMES /// ["All pills dispensed"]
+        /// Minimum:
+        // ? Ensure that the next "slot" is opto-fork, in order to confirm 'full revolution' ?
+        // Loop back to "STARTUP"
+        /// Advanced:
+        // LoRaWAN Report: Pills dispensed.
+        // LoRaWAN Report: Restarting operation.
     }
 }
