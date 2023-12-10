7b12203b079a047a6e1f4a1ccee91b4d46a0f4b9
Roni <roninu@metropolia.fi>
2023-12-10 20:34:26 +0200

    Added basic pwm functions.

--- LED/LED.h
+++ LED/LED.h
@@ -4,13 +4,30 @@
 
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "hardware/pwm.h"
+
+#define LED_1 22
+#define LED_2 21
+#define LED_3 20
+
+#define DIVIDER 125
+#define WRAP 999
+
+#define PWM_OFF 0
+#define PWM_SOFT 50 // 5%
+#define INIT_PWM_LEVEL PWM_OFF
 
 typedef struct {
     uint pin;
+    uint16_t pwm_level;
 } LED;
 
 void init_led(LED *led);
 bool check_led_state(LED *led);
 void blink_led(LED *led, bool state);
+
+void init_pwm(LED led);
+void put_pwm(LED * led, uint16_t level);
+void toggle_pwm(LED * led);
 
 #endif  // LED_H
