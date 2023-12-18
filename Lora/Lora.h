1fbfba4c7235555f2e7b0324cec33966aedc7bd2
Hoàng Phương Tạ <phuong.ta011290@gmail.com>
2023-12-18 13:25:08 +0200

    connect network and sending msg funtion modified!

---
+++ Lora/Lora.h
@@ -0,0 +1,30 @@
+
+#ifndef UART_H
+#define UART_H
+
+#include "hardware/uart.h"
+#include "pico/stdlib.h"
+#include <string.h>
+#include <stdio.h>
+
+#define UART_ID uart0
+#define BAUD_RATE 9600
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+#define DATA_BITS 8
+#define STOP_BITS 1
+#define PARITY    UART_PARITY_NONE
+#define STRLEN 80
+#define UART_WAIT_US 10000000
+
+void init_Lora();
+char* on_uart_rx();
+void start_lora();
+void set_time();
+void sync_real_time();
+void get_current_time();
+int get_current_second();
+void connect_network();
+void send_msg(char *content);
+
+#endif
