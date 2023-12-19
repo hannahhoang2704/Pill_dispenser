2da7c1374c326e64b2891c5de5d58a6b51cbfec4
Roni <roninu@metropolia.fi>
2023-12-19 16:00:08 +0200

    lots of stuff; preparing for adv. req. implementation; lots of it still under construction

--- operation/operation.c
+++ operation/operation.c
@@ -6,7 +6,7 @@
 
 // returns system time from boot
 // and changes time unit according to its size
-uint64_t get_time(char * time_unit) {
+uint64_t convert_time(char * time_unit) {
     uint16_t time_s = (time_us_64() >> 32) / 1000000;
     *time_unit = 's';
     if (time_s > 60) {
@@ -18,35 +18,33 @@ uint64_t get_time(char * time_unit) {
             *time_unit = 'm';
         }
     }
+    return time_s;
 }
 
-// Initialize all required pins
-void init_pins() {
-    printf("Initializing pins...\n");
-    init_switch(SW_0);
-    init_pwm();
-    init_opto_fork();
-    init_stepper();
-    init_piezo();
-    printf("Pins initialized.\n\n");
-    // log(PINS);
+// generates operations state,
+// data based on LoRa connection
+// and EEPROM data
+oper_st init_operation() {
+    start_lora();
+    oper_st state = {
+            .lora_conn = connect_network(),
+            .comp_rotd = 7, // get from EEPROM
+            .pills_det = 0}; // get from EEPROM
+    return state;
 }
 
 // Loops infinitely until switch is pressed,
 // blinking LED while looping.
 // Leaves LED off after press.
-void blink_until_sw_pressed(uint sw) {
-    // log(W8_BLINK);
-    uint8_t sw_i = abs(((int)sw - 7) - 2);
-    printf("Press switch #%u to calibrate...\n", sw_i);
+void blink_until_sw_pressed(SW sw, LED led) {
+    printf("Press switch #%u to calibrate...\n", sw.board_index);
     uint8_t loop = 0;
     while (!switch_pressed_debounced(sw)) {
-        if (loop++ == 0) toggle_pwm();
+        if (loop++ == 0) toggle_pwm(&led);
         loop %= 10;
         sleep_ms(50);
     }
-    printf("Switch %u pressed.\n\n", sw_i);
-    // log(PRESSED);
+    printf("Switch %u pressed.\n\n", sw.board_index);
 }
 
 // to_opto defines whether it will rotate in or out of opto-fork
@@ -65,17 +63,16 @@ int rotate_to_event(enum opto_events flag, bool clockwise) {
     return steps;
 }
 
-// Calibrates according to number of 'slots_left' done thus far.
-// 'slots_left' is to be derived from EEPROM
-void calibrate() {
-    // log(CALIB_START, slot_left);
+// Calibrates according to number of 'comp_rotd' done thus far.
+// 'comp_rotd' is to be derived from EEPROM
+void calibrate(oper_st * state) {
     printf("Calibrating...\n");
 
     int opto_width;
     int full_rev_minus_opto_width;
-    int full_rev_steps = 0;
+    int full_rev_steps;
 
-    if (state.slots_left == 0) {
+    if (state->comp_rotd == 7) {
 
         // rotate clockwise into opto-fork area
         rotate_to_event(FALL, true);
@@ -97,77 +94,80 @@ void calibrate() {
                "Opto-width: %d steps\n",
                full_rev_steps, opto_width);
 
+        state->comp_rotd = 0;
+
     } else {
 
-        // rotate counter-clockwise into opto-fork area
-        rotate_to_event(FALL, false);
+        // rotate counter-clockwise beyond opto-fork area
+        rotate_to_event(RISE, false);
+
+        sleep_ms(50);
 
         // rotate counter-clockwise out of opto-fork
         // and measure opto_width
-        opto_width = rotate_to_event(RISE, false);
+        opto_width = rotate_to_event(RISE, true);
 
-        // align the disk with the hole
-        // according to measured opto_width minus slippage
-        rotate_steps(-opto_width / 2 + 10);
-
-        // sleep to mitigate momentum,
-        // before rotating opposite direction
         sleep_ms(50);
 
+        // rotate counter-clockwise;
+        // align the disk with the hole
+        // which is half of opto_width + slippage
+        rotate_steps(-opto_width / 2 + 10);
+
         // then rotate according to logged
-        rotate_8th(state.slots_left);
+        rotate_8th(state->comp_rotd);
     }
     printf("Calibration finished!\n\n");
-    // log(CALIB_STOP, full_rev_steps, opto_width);
 }
 
 // Puts LED on.
 // Loops infinitely until switch is pressed.
 // Puts LED off after switch press.
-void wait_until_sw_pressed(uint sw) {
-    // log(W8);
-    uint8_t sw_i = abs(((int)sw - 7) - 2);
-    printf("Press switch #%u to start dispensing...\n", sw_i);
-    put_pwm(PWM_SOFT);
+void wait_until_sw_pressed(LED led, SW sw) {
+    printf("Press switch #%u to start dispensing...\n",
+           sw.board_index);
     while (!switch_pressed_debounced(sw)) {
         sleep_ms(50);
     }
-    printf("Switch #%u pressed.\n\n", sw_i);
-    put_pwm(PWM_OFF);
-    // log(PRESSED);
+    printf("Switch #%u pressed.\n\n",
+           sw.board_index);
 }
 
-// Dispense pills according 'slots_left',
+// Dispense pills according 'comp_rotd',
 // which is read from EEPROM on boot.
-void dispense() {
-    // log(DISP_1);
-    printf("Starting dispensing...\n"
-           "Slots left:          %u\n"
-           "Dispensing interval: %u s\n");
+void dispense(oper_st * state, LED led) {
 
+    printf("%s dispensing...\n",
+           state->comp_rotd == 0 ?
+           "Starting" :
+           "Continuing");
+
+    set_piezo_irq(true);
     uint64_t start = TIME_S;
-
-    for (int slot = 0; slot < slots_left; slot++) {
-
-        uint64_t next_pilling_time = start + PILL_INTERVAL_S * slot;
+    uint8_t comps_left = MAX_COMP_COUNT - state->comp_rotd;
+    for (uint8_t comp = 0; comp < comps_left; comp++) {
+        uint64_t next_pilling_time = start + PILL_INTERVAL_S * comp;
         while (TIME_S < next_pilling_time) {
             sleep_ms(5);
         }
 
+        // log(ROT_1);
         set_piezo_flag(false);
-        set_piezo_irq(true);
         rotate_8th(1);
+        // log(ROT_0);
 
         if (!piezo_detection_within_us()) {
-            printf("#%d No pill detected.\n", slot + 1);
-            led_blink_times(5);
+            printf("#%d No pill detected.\n", comp + 1);
+            // log(PILL_0, state);
+            led_blink_times(&led, BLINK_COUNT);
         } else {
-            printf("#%d Pill detected.\n", slot + 1);
-            ++pills_detected;
+            printf("#%d Pill detected.\n", comp + 1);
+            ++state->pills_det;
+            // log(PILL_0, state);
         }
-        set_piezo_irq(false);
-        // log(DISP_2)
+        ++state->comp_rotd;
     }
+
+    set_piezo_irq(false);
     printf("Dispensing finished.\n\n");
-    // log(DISP_0);
 }
--- operation/operation.h
+++ operation/operation.h
@@ -3,25 +3,60 @@
 #include "../LED/LED.h"
 #include "../switch/switch.h"
 #include "../piezo/piezo.h"
+#include "../Lora/Lora.h"
 
 #define TIME_S ((unsigned long) time_us_64() / 1000000)
 #define PILL_INTERVAL_S 10
-#define MAX_SLOT_COUNT 7
+#define MAX_COMP_COUNT 7 // comp = compartment
+#define BLINK_COUNT 5
 
-static struct state_s {
-    uint8_t slots_left;
-    uint8_t pills_detected;
-} state = {MAX_SLOT_COUNT,
-           0};
+static const char * log_msg[] =
+        {"Boot!",
+         "LoRa connection established!",
+         "LoRa connection failed...",
+         "State:\nCompartments left: %hhu\nPills detected: %hhu",
+         "Failed to read state from EEPROM\nResetting to default.",
+         "Switch #u pressed.",
+         "Printing logs.",
+         "Calibrating...",
+         "Calibration finished!\nFull revolution: %d steps.",
+         "Calibration finished!\nReset to compartment #%hhu",
+         "Dispensing started from compartment #%hhu",
+         "Dispensing finished!\nPills detected: %hhu",
+         "Starting rotation...",
+         "Rotation finished.",
+         "Pill detected!",
+         "No pill detected."};
 
-void init_pins();
+enum logs {
+    BOOT,
+    LORA_1,
+    LORA_0,
+    STATE_1,
+    STATE_0,
+    SW_PRESSED,
+    READ_LOG,
+    CALIB_1,
+    CALIB_0_1,
+    CALIB_0_2,
+    DISP_1,
+    DISP_0,
+    ROT_1,
+    ROT_0,
+    PILL_1,
+    PILL_0,
+};
 
-void blink_until_sw_pressed(uint sw);
+// contains program state information
+typedef struct operation_state {
+    bool lora_conn;
+    uint8_t comp_rotd; // compartments rotated for current dispense ; 7 = default
+    uint8_t pills_det; // pills detected during current dispense
+} oper_st;
 
+oper_st init_operation();
+void blink_until_sw_pressed(SW sw, LED led);
 int rotate_to_event(enum opto_events flag, bool clockwise);
-
-void calibrate();
-
-void wait_until_sw_pressed(uint sw);
-
-void dispense();
+void calibrate(oper_st * state);
+void wait_until_sw_pressed(LED led, SW sw);
+void dispense(oper_st * state, LED led);
