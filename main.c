b54b065603a1bb341c74ac5f96abc684b4b01bbc
Roni <roninu@metropolia.fi>
2023-12-20 15:27:27 +0200

    Added read_log at the end of the main loop.

--- main.c
+++ main.c
@@ -5,8 +5,6 @@
 int main() {
     stdio_init_all();
 
-    printf("\n%s\n", log_msg[BOOT]);
-
     LED led_3 = init_pwm(LED_3);
     SW sw_0 = init_switch(SW_0);
     init_opto_fork();
@@ -15,23 +13,20 @@ int main() {
     init_Lora();
     printf("Pins initialized.\n");
 
-    init_eeprom();
     oper_st state = init_operation();
-    print_state(state);
-
-    write_log_entry(log_msg[BOOT], &(state.eeprom_log_i));
+    //print_state(state);
 
     while (true) {
         if (state.comp_rotd == MAX_COMP_COUNT) {
             blink_until_sw_pressed(&sw_0, &led_3);
         }
-        write_log_entry(log_msg[SW_PRESSED], &(state.eeprom_log_i));
 
         calibrate(&state);
 
         wait_until_sw_pressed(&led_3, &sw_0);
-        write_log_entry(log_msg[SW_PRESSED], &(state.eeprom_log_i));
 
         dispense(&state, &led_3);
+
+        read_log_entry(MAX_ENTRIES);
     }
 }
