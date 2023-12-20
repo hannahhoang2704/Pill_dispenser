922d4e0ad23e226896568fdd80dda95b87fd1edd
Roni <roninu@metropolia.fi>
2023-12-20 11:56:38 +0200

    LoRa + EEPROM implementation; a crude version ; still under construction

--- main.c
+++ main.c
@@ -15,32 +15,23 @@ int main() {
     init_Lora();
     printf("Pins initialized.\n");
 
+    init_eeprom();
     oper_st state = init_operation();
-    initialize_eeprom();
+    print_state(state);
 
-    int rotations = 0; // read from eeprom, zero by default
-    uint8_t current_entry_index = 0;
-
-    read_from_eeprom(stored_rotation_address, (uint8_t *)&rotations, 1);
-    if (rotations < 0 || rotations > 7) rotations = 0;
-
-    //write first Boot log to memory
-    read_from_eeprom(stored_entry_index_address, &current_entry_index, 1);
-    if(current_entry_index >= MAX_ENTRIES || current_entry_index < 0) current_entry_index = 0;
-
-    char boot_log[] = "Boot";
-//    printf("%s\n", boot_log);
-    write_log_entry(boot_log, &current_entry_index, stored_entry_index_address);
+    write_log_entry(log_msg[BOOT], &(state.eeprom_log_i));
 
     while (true) {
         if (state.comp_rotd == MAX_COMP_COUNT) {
-            blink_until_sw_pressed(sw_0, led_3);
+            blink_until_sw_pressed(&sw_0, &led_3);
         }
+        write_log_entry(log_msg[SW_PRESSED], &(state.eeprom_log_i));
 
         calibrate(&state);
 
-        wait_until_sw_pressed(led_3, sw_0);
+        wait_until_sw_pressed(&led_3, &sw_0);
+        write_log_entry(log_msg[SW_PRESSED], &(state.eeprom_log_i));
 
-        dispense(&state, led_3);
+        dispense(&state, &led_3);
     }
 }
