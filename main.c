940f999c5eebb136fdcf2e413fe7819727f0c7c4
Roni <roninu@metropolia.fi>
2023-12-21 14:59:50 +0200

    Changed order of initializations. Nothing huge.

--- main.c
+++ main.c
@@ -5,25 +5,23 @@
 int main() {
     stdio_init_all();
 
+    oper_st state = init_operation();
     LED led_3 = init_pwm(LED_3);
-    SW sw_0 = init_switch(SW_0);
+    SW sw_proceed = init_switch(SW_0);
     init_opto_fork();
     init_stepper();
     init_piezo();
-    init_Lora();
-    printf("Pins initialized.\n");
 
-    oper_st state = init_operation();
     //print_state(state);
 
     while (true) {
         if (state.comp_rotd == MAX_COMP_COUNT) {
-            blink_until_sw_pressed(&sw_0, &led_3);
+            blink_until_sw_pressed(&sw_proceed, &led_3);
         }
 
         calibrate(&state);
 
-        wait_until_sw_pressed(&led_3, &sw_0);
+        wait_until_sw_pressed(&sw_proceed, &led_3);
 
         dispense(&state, &led_3);
 
