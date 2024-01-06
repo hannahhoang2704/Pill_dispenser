6de257b39178d669a7edd5369cd8b805af4194cb
Roni <roninu@metropolia.fi>
2024-01-06 13:53:39 +0200

    Removed unused function references. Moved flag-enum declaration to operation.h since it pertains to both opto AND piezo now.

--- opto-fork/opto.h
+++ opto-fork/opto.h
@@ -6,12 +6,5 @@
 #define OPTO_GPIO 28
 #define EVENT_DEBOUNCE_US 5000
 
-enum opto_events {
-    FALL,
-    RISE
-};
-
-bool opto_flag_state(enum opto_events event);
-void set_opto_flag(enum opto_events event, bool state);
 void init_opto_fork();
 #endif
